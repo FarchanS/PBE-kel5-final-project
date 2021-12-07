d871eaf82b3992e0b48fb15d8e1dafc8f6cd72ca
FarchanS <93101895+FarchanS@users.noreply.github.com>
2021-12-08 05:43:27 +0800

    add script to call another form which more safer
    located inside push button "network tool"

--- main_prog.py
+++ main_prog.py
@@ -2,6 +2,8 @@
 from Main import *
 from PyQt5.QtGui import *
 import os
+from CalculatorWindow import *
+from Calculator_prog import *
 
 def OpenCalculatorWindow(self):
     # AboutUs_Window.hide()
@@ -18,19 +20,31 @@ def OpenNetworkToolWindow(self):
 
 def OpenAboutUsWindow(self):
     os.system('AboutUsWindow_prog.py')
+
+def CalcShow(self):
+    self.window = QtWidgets.QMainWindow()
+    self.ui = Ui_CalculatorWindow()
+    self.ui.setupUi(self.window) 
+    self.ui.signals()
+    self.window.show()
+    # Main.hide()
+    print("signal")
     
 
 def signals(self):
     self.PB_Kalkulator.clicked.connect(lambda: self.OpenCalculatorWindow())
     self.PB_EngineeringTool.clicked.connect(lambda: self.OpenEngineeringWindow())
-    self.PB_NetworkTool.clicked.connect(lambda: self.OpenNetworkToolWindow())
+    self.PB_NetworkTool.clicked.connect(self.CalcShow)
+    # self.PB_NetworkTool.clicked.connect(lambda: self.OpenNetworkToolWindow())
     self.PB_AboutUs.clicked.connect(lambda: self.OpenAboutUsWindow())
+    # self.pushButton_2.clicked.connect(self.CalcShow)
 
 Ui_Main.signals = signals
 Ui_Main.OpenCalculatorWindow = OpenCalculatorWindow
 Ui_Main.OpenEngineeringWindow = OpenEngineeringWindow
 Ui_Main.OpenNetworkToolWindow = OpenNetworkToolWindow
 Ui_Main.OpenAboutUsWindow = OpenAboutUsWindow
+Ui_Main.CalcShow = CalcShow
 
 if __name__ == "__main__":              
     import sys
